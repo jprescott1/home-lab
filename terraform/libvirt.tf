b54c814fbe9c8e78b1fb46b70b3f858611f4420f
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-15 17:50:31 -0600

    lets try again

--- terraform/libvirt.tf
+++ terraform/libvirt.tf
@@ -27,7 +27,7 @@ resource "libvirt_volume" "ubuntu-qcow2" {
 }
 
 resource "libvirt_cloudinit_disk" "commoninit" {
-  name           = "commoninit.iso"
+  name           = "commoninit2.iso"
   user_data      = data.template_file.user_data.rendered
   network_config = data.template_file.network_config.rendered
   pool           = libvirt_pool.ubuntu.name
