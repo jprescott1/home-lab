682399ca136cfdb8342ea79e40edb1d45946576c
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 17:43:40 -0600

    create one vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,6 @@ terraform {
   }
 }
 
-module "worker_node" {
-  source   = "github.com/jprescott1/terraform-libvirt-module"
-  vm_name  = "worker-node"
-  vm_count = 3
-  memory   = 4096
-  vcpu     = 2
-}
-
 module "control_plane" {
   source   = "github.com/jprescott1/terraform-libvirt-module"
   vm_name  = "control-plane"
