b671601b491e911f653514ec9a3612637a441040
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 18:02:56 -0600

    Try removing a vm

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ terraform {
 module "control_plane" {
   source   = "github.com/jprescott1/terraform-libvirt-module"
   vm_name  = "control-plane"
-  vm_count = 2
+  vm_count = 1
   memory   = 4096
   vcpu     = 2
 }
