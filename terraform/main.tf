9ce276a9c7d8ff859206a63a080205d8b4ef7698
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 19:09:10 -0600

    wipe it

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,10 @@ terraform {
   }
 }
 
-module "control_plane" {
-  source   = "github.com/jprescott1/terraform-libvirt-module"
-  vm_name  = "control-plane"
-  vm_count = 1
-  memory   = 4096
-  vcpu     = 2
-}
-
-module "worker_node" {
-  source   = "github.com/jprescott1/terraform-libvirt-module"
-  vm_name  = "control-plane"
-  vm_count = 3
-  memory   = 4096
-  vcpu     = 2
-}
+# module "control_plane" {
+#   source   = "github.com/jprescott1/terraform-libvirt-module"
+#   vm_name  = "control-plane"
+#   vm_count = 1
+#   memory   = 4096
+#   vcpu     = 2
+# }
