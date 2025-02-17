87856bfba7e90c02661b80bc8f31245083a24a2c
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 19:20:19 -0600

    lets try a different module

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,18 +5,29 @@ terraform {
   }
 }
 
-module "control_plane" {
-  source   = "github.com/jprescott1/terraform-libvirt-module"
-  vm_name  = "control-plane"
-  vm_count = 1
-  memory   = 4096
-  vcpu     = 2
+provider "libvirt" {
+  uri = "qemu:///system"
 }
 
-module "worker_node" {
-  source   = "github.com/jprescott1/terraform-libvirt-module"
-  vm_name  = "control-plane"
-  vm_count = 3
-  memory   = 4096
-  vcpu     = 2
+module "test_nodes" {
+  source             = "MonolithProjects/vm/libvirt"
+  vm_hostname_prefix = "test"
+  autostart          = false
+  vm_count           = 1
+  index_start        = 1
+  memory             = "512"
+  vcpu               = 1
+  system_volume      = 20
+  graphics           = "vnc"
+  ssh_admin          = "admin"
+  ssh_private_key    = "~/.ssh/id_ed25519"
+  ssh_keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIJ2/7bvjNBsr/VGNJTbgx9UPyCPjwxf8Ie8d30a4+l jimmy@lab-host",
+  ]
+  local_admin        = "localadmin"
+  local_admin_passwd = "<yout password hash (mkpasswd --method=SHA-512 --rounds=4096)>"
+}
+
+output "output_data" {
+  value = module.test_nodes
 }
