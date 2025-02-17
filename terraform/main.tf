b32038fff061700ecfa5961fd24b5beb607cc913
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 18:38:21 -0600

    revert

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,14 +5,6 @@ terraform {
   }
 }
 
-resource "libvirt_pool" "ubuntu" {
-  name = "cluster"
-  type = "dir"
-  target {
-    path = "/var/lib/libvirt/cluster_images"
-  }
-}
-
 module "control_plane" {
   source    = "github.com/jprescott1/terraform-libvirt-module"
   vm_name   = "control-plane"
