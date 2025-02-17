83ab9265b6c44393566235b459f4b1739aefa178
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 18:39:33 -0600

    revert

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,9 @@ terraform {
 }
 
 module "control_plane" {
-  source    = "github.com/jprescott1/terraform-libvirt-module"
-  vm_name   = "control-plane"
-  vm_count  = 1
-  memory    = 4096
-  vcpu      = 2
+  source   = "github.com/jprescott1/terraform-libvirt-module"
+  vm_name  = "control-plane"
+  vm_count = 1
+  memory   = 4096
+  vcpu     = 2
 }
