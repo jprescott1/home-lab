d73454553975c884de2af959ce141300d01da481
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 19:21:25 -0600

    lets try a different module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = ">= 0.13"
+    required_providers {
+      libvirt = {
+        source  = "dmacvicar/libvirt"
+      }
+    }
+}
+
+terraform {
   backend "gcs" {
     bucket = "gha-lab-bucket"
     prefix = "./tf-deploy"
