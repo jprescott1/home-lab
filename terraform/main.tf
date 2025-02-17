d53721e49299778e10af7e5a86421c68108b47ba
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 18:17:39 -0600

    try moving pools to main

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,19 @@ terraform {
   }
 }
 
+resource "libvirt_pool" "ubuntu" {
+  name = "cluster"
+  type = "dir"
+  target {
+    path = "/var/lib/libvirt/cluster_images"
+  }
+}
+
 module "control_plane" {
   source   = "github.com/jprescott1/terraform-libvirt-module"
   vm_name  = "control-plane"
   vm_count = 1
   memory   = 4096
   vcpu     = 2
+  pool_name = libvirt_pool.ubuntu.name
 }
