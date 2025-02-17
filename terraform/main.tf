449fa71906dad88d8690728fcfd14c79a0743c7e
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-16 19:22:05 -0600

    lets try a different module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 0.13"
-    required_providers {
-      libvirt = {
-        source  = "dmacvicar/libvirt"
-      }
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
     }
+  }
 }
 
 terraform {
