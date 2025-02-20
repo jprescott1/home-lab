005c1fb28236a7eb1ffdb75170a244848726fa6f
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-20 10:35:02 -0600

    debug

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,16 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
+output "path_debug" {
+  value = "${path.module}/sources/guest.qcow2"
+}
+
+output "path_debug" {
+  value = "${path.module}/guest.qcow2"
+}
+
 resource "libvirt_pool" "vmpool" {
-  name = "cloud-pool"
+  name = "default"
   type = "dir"
   target {
     path = "${path.module}/volume"
@@ -22,7 +30,7 @@ resource "libvirt_pool" "vmpool" {
 resource "libvirt_volume" "vm-qcow2" {
   name   = "guest.qcow2"
   pool   = libvirt_pool.vmpool.name
-  source = "${path.module}/sources/guest.qcow2"
+  source = "${path.module}/guest.qcow2"
   format = "qcow2"
 }
 
