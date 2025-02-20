bad01abaf7541d9489f185e1a40ea2236c7d4f42
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-20 10:25:43 -0600

    not going to work

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,21 +11,6 @@ provider "libvirt" {
   uri = "qemu:///system"
 }
 
-resource "libvirt_pool" "vmpool" {
-  name = "cloud-pool"
-  type = "dir"
-  target {
-    path = "${path.module}/volume"
-  }
-}
-
-resource "libvirt_volume" "vm-qcow2" {
-  name   = "guest.qcow2"
-  pool   = libvirt_pool.vmpool.name
-  source = "${path.module}/sources/guest.qcow2"
-  format = "qcow2"
-}
-
 module "vm" {
   source  = "MonolithProjects/vm/libvirt"
   version = "1.12.0"
@@ -34,7 +19,6 @@ module "vm" {
   vm_count           = 1
   memory             = "2048"
   vcpu               = 1
-  pool               = "cloud-pool"
   system_volume      = 20
   dhcp               = true
   local_admin        = "local-admin"
