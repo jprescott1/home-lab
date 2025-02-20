83f1eb3f7a62b34ce283bce90f45d3781ca3eaa3
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-20 13:54:26 -0600

    lets try multiple vms

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,10 +37,10 @@ module "vm" {
   source  = "MonolithProjects/vm/libvirt"
   version = "1.12.0"
 
-  vm_hostname_prefix = "server"
-  vm_count           = 1
-  memory             = "2048"
-  vcpu               = 1
+  vm_hostname_prefix = "control-plane"
+  vm_count           = 3
+  memory             = "4000"
+  vcpu               = 2
   system_volume      = 20
   dhcp               = true
   ssh_admin          = "jimmy"
@@ -48,6 +48,25 @@ module "vm" {
   ssh_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqCNHKusEfJmWp7PQcGhgFWBWAq3RBKn9dXoZJMO+Ri jimmy@dev-lab",
   ]
-  time_zone  = "CET"
+  time_zone  = "UTC"
+  os_img_url = "file:///home/jimmy/images/ubuntu-22.04-server-cloudimg-amd64.img"
+}
+
+module "vm" {
+  source  = "MonolithProjects/vm/libvirt"
+  version = "1.12.0"
+
+  vm_hostname_prefix = "worker-node"
+  vm_count           = 5
+  memory             = "2000"
+  vcpu               = 2
+  system_volume      = 20
+  dhcp               = true
+  ssh_admin          = "jimmy"
+  ssh_private_key    = "/home/jimmy/.ssh/id_ed25519"
+  ssh_keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqCNHKusEfJmWp7PQcGhgFWBWAq3RBKn9dXoZJMO+Ri jimmy@dev-lab",
+  ]
+  time_zone  = "UTC"
   os_img_url = "file:///home/jimmy/images/ubuntu-22.04-server-cloudimg-amd64.img"
 }
