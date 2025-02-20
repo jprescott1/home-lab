b5581d5ef59dabf16329772516b0fa4b8f4ff590
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-02-20 09:33:28 -0600

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
 terraform {
   required_version = ">= 0.13"
-    required_providers {
-      libvirt = {
-        source  = "dmacvicar/libvirt"
-      }
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
     }
+  }
 }
 
 provider "libvirt" {
@@ -16,21 +16,21 @@ module "vm" {
   version = "1.12.0"
 
   vm_hostname_prefix = "server"
-  vm_count    = 1
-  memory      = "2048"
-  vcpu        = 1
-  pool        = "terra_pool"
-  system_volume = 20
-  dhcp        = true
-  local_admin = "local-admin"
-  ssh_admin   = "ci-user"
-  ssh_private_key = "~/.ssh/id_ed25519"
+  vm_count           = 1
+  memory             = "2048"
+  vcpu               = 1
+  pool               = "terra_pool"
+  system_volume      = 20
+  dhcp               = true
+  local_admin        = "local-admin"
+  ssh_admin          = "ci-user"
+  ssh_private_key    = "~/.ssh/id_ed25519"
   local_admin_passwd = "$6$rounds=4096$XB9Gy/7WNzwW6MgO$b/WqUk9vcs/sblEz4jJ2omW6/rmaX9oausQfVozVnnCHyrXFTnRYS7gmq5emWFreZ1Ddqeq.qqK83z88pbs9C1"
-  ssh_keys    = [
+  ssh_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAsycFZCGi6778LPkAq2I9RJlmkNrMwEEiZvGwWp5tvg jimmyjorts@gloogleegloo.com",
-    ]
-  time_zone   = "CET"
-  os_img_url  = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
+  ]
+  time_zone  = "CET"
+  os_img_url = "https://cloud-images.ubuntu.com/releases/24.04/release/ubuntu-24.04-server-cloudimg-amd64.img"
 }
 
 output "ip_addresses" {
