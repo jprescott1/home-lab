9041edd53a5dfa62ebd847572ebb530f4bf8e1a4
Jimmy Prescott <bunches_stands_0s@icloud.com>
2025-04-19 07:34:24 -0500

    static ips

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,11 +43,11 @@ module "vm1" {
   vcpu               = 4
   system_volume      = 100
   dhcp               = false
-  ip_address = [
-    "192.168.10.54"
-  ]
-  ssh_admin       = "jimmy"
-  ssh_private_key = "/home/jimmy/.ssh/id_ed25519"
+  ip_address         = "192.168.10.54/24"
+  ip_gateway         = "192.168.10.1"
+  ip_nameserver      = ["8.8.8.8", "8.8.4.4"]
+  ssh_admin          = "jimmy"
+  ssh_private_key    = "/home/jimmy/.ssh/id_ed25519"
   ssh_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqCNHKusEfJmWp7PQcGhgFWBWAq3RBKn9dXoZJMO+Ri jimmy@dev-lab",
   ]
@@ -64,15 +64,9 @@ module "vm2" {
   memory             = "4096"
   vcpu               = 4
   system_volume      = 20
-  dhcp               = false
-  ip_address = [
-    "192.168.10.50",
-    "192.168.10.51",
-    "192.168.10.52",
-    "192.168.10.53"
-  ]
-  ssh_admin       = "jimmy"
-  ssh_private_key = "/home/jimmy/.ssh/id_ed25519"
+  dhcp               = true
+  ssh_admin          = "jimmy"
+  ssh_private_key    = "/home/jimmy/.ssh/id_ed25519"
   ssh_keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIqCNHKusEfJmWp7PQcGhgFWBWAq3RBKn9dXoZJMO+Ri jimmy@dev-lab",
   ]
